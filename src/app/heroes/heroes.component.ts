fe476a5e1930971bca9823e6ed0ea3f23d3e422b
Ben Baacke <ben.baacke@gmail.com>
2026-01-16 13:12:34 -0500

    Step 2: Display list

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,18 +1,21 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
-import { UpperCasePipe } from '@angular/common';
-import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
+import { NgFor, UpperCasePipe, NgIf } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
   standalone: true,
-  imports: [UpperCasePipe, FormsModule],
+  imports: [FormsModule, NgFor, UpperCasePipe, NgIf],
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm',
-  };
+  heroes = HEROES;
+  selectedHero?: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
